3a759ae4a5bc50935a249ee00ee951d5b77ef835
Matheus Alencar <matheusalencar6942@gmail.com>
2022-12-22 14:04:31 -0300

    return on page not found

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
-  ngOnInit(): void {
+  back(): void {
+    this._location.back();
   }
 
 }
