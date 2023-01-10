3b663f0628ec48c306eeb0bae636346ba793f861
Matheus Alencar <matheusalencar6942@gmail.com>
2023-01-09 22:22:39 -0300

    subscription para evitar memory leak

--- src/app/components/input-search/input-search.component.ts
+++ src/app/components/input-search/input-search.component.ts
@@ -1,26 +1,39 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { BehaviorSubject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { BehaviorSubject, debounceTime, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-input-search',
   templateUrl: './input-search.component.html',
   styleUrls: ['./input-search.component.scss'],
 })
-export class InputSearchComponent implements OnInit {
+export class InputSearchComponent implements OnInit, OnDestroy {
   searchTerm: string = '';
   private _searchTerm$: BehaviorSubject<string> = new BehaviorSubject<string>(
     ''
   );
+  private _subscription: Subscription = new Subscription();
 
   @Output() emitSearchTerm: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this._searchTerm$.pipe(debounceTime(500)).subscribe((term) => {
-      this.emitSearchTerm.emit(term);
-    });
+    this._subscription = this._searchTerm$
+      .pipe(debounceTime(500))
+      .subscribe((term) => {
+        this.emitSearchTerm.emit(term);
+      });
   }
 
   changeSearchTerm(event: string): void {
     this._searchTerm$.next(event);
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
   }
 }
