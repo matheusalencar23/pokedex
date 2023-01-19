d72908e21029f95b3ee7d6eabbc09594bbba8e05
Matheus Alencar <matheusalencar6942@gmail.com>
2023-01-19 07:48:32 -0300

    ajuste botão de carregar mais

--- src/app/components/load-more-button/load-more-button.component.ts
+++ src/app/components/load-more-button/load-more-button.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-load-more-button',
   templateUrl: './load-more-button.component.html',
-  styleUrls: ['./load-more-button.component.scss']
+  styleUrls: ['./load-more-button.component.scss'],
 })
 export class LoadMoreButtonComponent implements OnInit {
+  @Output() loadMore = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
