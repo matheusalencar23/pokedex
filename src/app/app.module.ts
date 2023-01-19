b9ad3a11d26ddb2bd72e45f17c2df81647d3a99e
Matheus Alencar <matheusalencar6942@gmail.com>
2023-01-19 08:20:31 -0300

    animação do modal usando angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { PageNotFoundModule } from './components/page-not-found/page-not-found.m
     AppRoutingModule,
     PageNotFoundModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,9 +1,34 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
+  animations: [
+    trigger('overlay', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms', style({ opacity: 0.5 })),
+      ]),
+      transition(':leave', [animate('500ms', style({ opacity: 0 }))]),
+    ]),
+    trigger('modal', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5) translate(-50%, -50%)', opacity: 0 }),
+        animate(
+          '250ms',
+          style({ transform: 'scale(1) translate(-50%, -50%)', opacity: 1 })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '250ms',
+          style({ transform: 'scale(0.5) translate(-50%, -50%)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ModalComponent implements OnInit {
   @Input() visible: boolean = true;
