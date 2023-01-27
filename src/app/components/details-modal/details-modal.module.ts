bae36c529ee5626e8e8564daf6bc71bd39894589
Matheus Alencar <matheusalencar6942@gmail.com>
2023-01-26 22:41:44 -0300

    Cores e responsividade do modal de detalhes

--- src/app/components/details-modal/details-modal.module.ts
+++ src/app/components/details-modal/details-modal.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsModalComponent } from './details-modal.component';
+import { CapitalizeModule } from 'src/app/pipes/capitalize/capitalize.module';
 
 @NgModule({
   declarations: [DetailsModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CapitalizeModule],
   exports: [DetailsModalComponent],
 })
 export class DetailsModalModule {}
