15dbf9339a0f053878333bd7701ddeb3e63c2b06
Matheus Alencar <matheus.alencar@avitaseg.com.br>
2023-03-07 17:20:51 -0300

    refatorando interfaces dos pokemons

---
+++ src/app/models/base.ts
@@ -0,0 +1,4 @@
+export interface IBase {
+  name: string;
+  url: string;
+}
--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -1,7 +1,4 @@
-interface IBase {
-  name: string;
-  url: string;
-}
+import { IBase } from './base';
 
 export interface ISimplePokemon extends IBase {}
 
@@ -13,16 +10,10 @@ export interface APISimplePokemon {
 }
 
 export interface IPokemon {
-  abilities: Ability[];
   base_experience: number;
   forms: IBase[];
-  game_indices: Index[];
   height: number;
-  held_items: HeldItem[];
   id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves: Mfe[];
   name: string;
   order: number;
   past_types: any[];
@@ -31,38 +22,6 @@ export interface IPokemon {
   stats: Stat[];
   types: Type[];
   weight: number;
-}
-
-interface Ability {
-  ability: IBase;
-  is_hidden: boolean;
-  slot: number;
-}
-
-interface Index {
-  game_index: number;
-  version: IBase;
-}
-
-interface HeldItem {
-  item: IBase;
-  version_details: VersionDetail[];
-}
-
-interface VersionDetail {
-  rarity: number;
-  version: IBase;
-}
-
-interface Mfe {
-  move: IBase;
-  version_group_details: VersionGroupDetail[];
-}
-
-interface VersionGroupDetail {
-  level_learned_at: number;
-  move_learn_method: IBase;
-  version_group: IBase;
 }
 
 interface Sprites {
@@ -75,7 +34,6 @@ interface Sprites {
   front_shiny: string;
   front_shiny_female: any;
   other: Other;
-  versions: Versions;
 }
 
 interface Other {
@@ -99,209 +57,6 @@ interface Home {
 interface OfficialArtwork {
   front_default: string;
   front_shiny: string;
-}
-
-interface Versions {
-  'generation-i': GenerationI;
-  'generation-ii': GenerationIi;
-  'generation-iii': GenerationIii;
-  'generation-iv': GenerationIv;
-  'generation-v': GenerationV;
-  'generation-vi': GenerationVi;
-  'generation-vii': GenerationVii;
-  'generation-viii': GenerationViii;
-}
-
-interface GenerationI {
-  'red-blue': RedBlue;
-  yellow: Yellow;
-}
-
-interface RedBlue {
-  back_default: string;
-  back_gray: string;
-  back_transparent: string;
-  front_default: string;
-  front_gray: string;
-  front_transparent: string;
-}
-
-interface Yellow {
-  back_default: string;
-  back_gray: string;
-  back_transparent: string;
-  front_default: string;
-  front_gray: string;
-  front_transparent: string;
-}
-
-interface GenerationIi {
-  crystal: Crystal;
-  gold: Gold;
-  silver: Silver;
-}
-
-interface Crystal {
-  back_default: string;
-  back_shiny: string;
-  back_shiny_transparent: string;
-  back_transparent: string;
-  front_default: string;
-  front_shiny: string;
-  front_shiny_transparent: string;
-  front_transparent: string;
-}
-
-interface Gold {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-  front_transparent: string;
-}
-
-interface Silver {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-  front_transparent: string;
-}
-
-interface GenerationIii {
-  emerald: Emerald;
-  'firered-leafgreen': FireredLeafgreen;
-  'ruby-sapphire': RubySapphire;
-}
-
-interface Emerald {
-  front_default: string;
-  front_shiny: string;
-}
-
-interface FireredLeafgreen {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-}
-
-interface RubySapphire {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-}
-
-interface GenerationIv {
-  'diamond-pearl': DiamondPearl;
-  'heartgold-soulsilver': HeartgoldSoulsilver;
-  platinum: Platinum;
-}
-
-interface DiamondPearl {
-  back_default: string;
-  back_female: any;
-  back_shiny: string;
-  back_shiny_female: any;
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface HeartgoldSoulsilver {
-  back_default: string;
-  back_female: any;
-  back_shiny: string;
-  back_shiny_female: any;
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface Platinum {
-  back_default: string;
-  back_female: any;
-  back_shiny: string;
-  back_shiny_female: any;
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface GenerationV {
-  'black-white': BlackWhite;
-}
-
-interface BlackWhite {
-  animated: Animated;
-  back_default: string;
-  back_female: any;
-  back_shiny: string;
-  back_shiny_female: any;
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface Animated {
-  back_default: string;
-  back_female: any;
-  back_shiny: string;
-  back_shiny_female: any;
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface GenerationVi {
-  'omegaruby-alphasapphire': OmegarubyAlphasapphire;
-  'x-y': XY;
-}
-
-interface OmegarubyAlphasapphire {
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface XY {
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface GenerationVii {
-  icons: Icons;
-  'ultra-sun-ultra-moon': UltraSunUltraMoon;
-}
-
-interface Icons {
-  front_default: string;
-  front_female: any;
-}
-
-interface UltraSunUltraMoon {
-  front_default: string;
-  front_female: any;
-  front_shiny: string;
-  front_shiny_female: any;
-}
-
-interface GenerationViii {
-  icons: Icons2;
-}
-
-interface Icons2 {
-  front_default: string;
-  front_female: any;
 }
 
 interface Stat {
